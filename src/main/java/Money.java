f74e92fa3c20f9526e079bdf9c716f7db45adcfd
오진주 <jinjuoh@hancom.com>
2021-06-03 16:18:13 +0900

    13

---
+++ src/main/java/Money.java
@@ -0,0 +1,43 @@
+class Money implements Expression {
+
+    protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount && currency().equals(money.currency());
+    }
+
+    static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
+    }
+
+    String currency() {
+        return currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+}
